fa5971b918efbd2e3719d9c99ad7e11d1472d773
LordKelvinHelmholtz <johnanthonycicco@gmail.com>
2025-11-20 15:29:57 -0500

    level map update

--- game1/Assets/Scripts/player_controller.gd
+++ game1/Assets/Scripts/player_controller.gd
@@ -2,7 +2,10 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+#how fast you move
 const JUMP_VELOCITY = -400.0
+#how fast you fall back down
+const RUN_MULTIPLIER = 1.5
 
 
 func _physics_process(delta: float) -> void:
@@ -17,9 +20,13 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
+	var current_speed = SPEED
+	if Input.is_action_pressed("run"):
+		current_speed*=RUN_MULTIPLIER
+		
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, current_speed)
 
 	move_and_slide()
